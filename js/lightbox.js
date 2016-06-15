01eea170b8c2c7f75cf267205b7a8cad059de36a
see365 <see365@qq.com>
2016-06-15 15:02:42 +0800

    add

---
+++ js/lightbox.js
@@ -0,0 +1,16 @@
+/**
+ * a micro light box Created by heyong on 16/6/15.
+ * need import jquery
+ * version 0.1.0
+ */
+(function(window,$){
+    var slice=[].slice;
+
+    function LightBox(){
+
+    }
+    LightBox.fn=LightBox.prototype={
+
+    }
+
+})(window,$);
